8b2662e7ea571fb86e2d683795d5037e11310494
Matthew Tucker <tuckmn1@student.op.ac.nz>
2017-05-25 10:31:42 +1200

    Added this weeks practical

---
+++ Assignment4/react/script.js
@@ -0,0 +1,39 @@
+var products = [{
+  key: 1,	
+  name: 'Cookies',
+  source: './images/cookies.png',
+  price: 3.29
+}, {
+  key: 2,	
+  name: 'Ice cream',
+  source: "images/icecream.png",
+  price: 4.99
+}, {
+  key: 3,	
+  name: 'Chips',
+  source: "./images/chips.png",
+  price: 2.29
+}, {
+  key: 4,	
+  name: 'Donuts',
+  source: "./images/donuts.png",
+  price: 5.55
+}
+, {
+  key: 5,	
+  name: 'Chocolate',
+  source: "./images/chocolate.png",
+  price: 4.79
+}
+
+, {
+  key: 6,	
+  name: 'Candy',
+  source: "./images/candy.png",
+  price: 3.40
+}
+
+];
+
+
+
